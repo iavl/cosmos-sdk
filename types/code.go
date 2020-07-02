3dc0e080d225933264c79baf383e9118d129a25b
iavl <z6521256@gmail.com>
2020-07-02 16:08:56 +0800

    add Code and Storage for evm

---
+++ types/code.go
@@ -0,0 +1,55 @@
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/hexutil"
+)
+
+type (
+	Code    []byte
+	Storage map[Hash]Hash
+)
+
+func (c Code) String() string {
+	return hexutil.Encode(c)
+}
+
+func (c Code) MarshalJSON() ([]byte, error) {
+	return json.Marshal(c.String())
+}
+
+func (c *Code) UnmarshalJSON(data []byte) error {
+	var s string
+	err := json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+
+	d, err := hexutil.Decode(s)
+
+	if err != nil {
+		return err
+	}
+
+	*c = d
+
+	return nil
+}
+
+func (s Storage) String() (str string) {
+	for key, value := range s {
+		str += fmt.Sprintf("%X : %X\n", key, value)
+	}
+	return
+}
+
+func (s Storage) Copy() Storage {
+	cpy := make(Storage)
+	for key, value := range s {
+		cpy[key] = value
+	}
+
+	return cpy
+}
